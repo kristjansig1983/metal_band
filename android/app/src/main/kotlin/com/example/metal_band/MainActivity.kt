6a99870d805f7d49aae169ad5c1b8e4bcb7ffbd5
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-04-08 14:23:01 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/metal_band/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.metal_band
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+  MaterialApp(
+    home: StagePage(),
+  ),
+);
+
+class StagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      appBar: AppBar(
+        titleTextStyle: TextStyle(
+          fontFamily: 'RubicWatPaint-Regular',
+          fontSize: 20,
+        ),
+        backgroundColor: Colors.black,
+        title: Text('What Metal Band Are You?'),
+      ),
+      body: Stage(),
+    );
+  }
+}
+
+class Stage extends StatefulWidget {
+  @override
+  State<Stage> createState() => _StageState();
+}
+
+class _StageState extends State<Stage> {
+  void changeAnswer() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  int ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        //Stops image flashing
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onPressed: () {
+          changeAnswer();
+        },
+        child: Image.asset('images/stage$ballNumber.png'),
+      ),
+    );
+  }
+}
